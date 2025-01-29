14f64d9d13f80b13aeaf8364763029d334f0699b
Sari Amin <sariamin435@gmail.com>
2025-01-29 12:08:12 +0300

    trying to test it for all setions

--- src/news_scraper.py
+++ src/news_scraper.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 data = []
 
-url = "https://www.bbc.com/news"
+url = "https://www.bbc.com/sport"
 
 try:
     response = requests.get(url)
@@ -15,39 +15,40 @@ except requests.exceptions.RequestException as e:
 
 soup = BeautifulSoup(response.content, "html.parser")
 
-article = soup.find("article")
+articles = soup.find_all("article")
 
-if article:
-    news = article.find_all("a", class_ = "sc-2e6baa30-0 gILusN")
+for article in articles:
+    if article:
+        news = article.find_all("a", class_ = "sc-2e6baa30-0 gILusN")
 
-    for new in news:
-        newsUrl = new.get("href")
+        for new in news:
+            newsUrl = new.get("href")
+            print(newsUrl)
+            if newsUrl.startswith("/"):
+                newsUrl = "https://www.bbc.com" + newsUrl
 
-        if newsUrl.startswith("/"):
-            newsUrl = "https://www.bbc.com" + newsUrl
+                newsHeadline = new.get_text()
 
-            newsHeadline = new.get_text()
+                try:
+                    newsResponse = requests.get(newsUrl)
+                    newsResponse.raise_for_status()
+                except requests.exceptions.RequestException as e:
+                    print(f"An error occurred while fetching {newsUrl}: {e}")
+                    continue
 
-            try:
-                newsResponse = requests.get(newsUrl)
-                newsResponse.raise_for_status()
-            except requests.exceptions.RequestException as e:
-                print(f"An error occurred while fetching {newsUrl}: {e}")
-                continue
+                newsSoup = BeautifulSoup(newsResponse.content, "html.parser")
+                newsSummary = newsSoup.find('meta', {"name": "description"})
 
-            newsSoup = BeautifulSoup(newsResponse.content, "html.parser")
-            newsSummary = newsSoup.find('meta', {"name": "description"})
+                newsSummary = newsSummary["content"] if newsSummary else "No summary available."
 
-            newsSummary = newsSummary["content"] if newsSummary else "No summary available."
+                newRow = {
+                    "Headline" : newsHeadline,
+                    "Summary" : newsSummary,
+                    "Url" : newsUrl
+                }
 
-            newRow = {
-                "Headline" : newsHeadline,
-                "Summary" : newsSummary,
-                "Url" : newsUrl
-            }
-
-            data.append(newRow)
+                data.append(newRow)
 
 data = pd.DataFrame(data)
 
-data.to_csv("data/headlines.csv", index = False)
+data.to_csv("data/sport.csv", index = False)
